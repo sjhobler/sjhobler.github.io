3728bf5327ab3048301423690dae7780e6ed5cfc
sjhobler <86021472+sjhobler@users.noreply.github.com>
2025-10-30 18:44:31 +0000

    Deploying to gh-pages from @ sjhobler/sjhobler.github.io@f0d5f94e7e7830df41ed1f538d276ccd8234493d 🚀

--- assets/js/photoswipe-setup.js
+++ assets/js/photoswipe-setup.js
@@ -1,5 +1,5 @@
-import PhotoSwipeLightbox from "https://cdn.jsdelivr.net/npm/photoswipe@5.4.4/dist/photoswipe-lightbox.esm.min.js";
-import PhotoSwipe from "https://cdn.jsdelivr.net/npm/photoswipe@5.4.4/dist/photoswipe.esm.min.js";
+import PhotoSwipeLightbox from "/assets/libs/photoswipe-lightbox/photoswipe-lightbox.esm.min.js";
+import PhotoSwipe from "/assets/libs/photoswipe/photoswipe.esm.min.js";
 const photoswipe = new PhotoSwipeLightbox({
   gallery: ".pswp-gallery",
   children: "a",
